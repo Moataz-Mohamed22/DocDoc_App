36b7e98488bb9eb17f9e47caa1979adc688fad5d
Moataz <moataz.wmm@gmail.com>
2025-04-29 15:10:43 +0300

    added Production &Development flavors for Android

---
+++ lib/dev_main.dart
@@ -0,0 +1,19 @@
+import 'package:dr_app/core/routing/app_router.dart';
+import 'package:dr_app/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/di/di.dart';
+import 'core/utils/bloc_observer.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
+  Bloc.observer = MyBlocObserver();
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(DocApp(appRouter: AppRouter(),));
+}
+
+
+
